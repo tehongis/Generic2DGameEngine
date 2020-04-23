573a05a73c0e49ececf6f194e32be9d39a95d2ed
Tero Hongisto <thongisto@codebian.dc.net>
2020-04-23 22:42:59 +0300

    SDL init

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 
 #include <iostream>
 
+#include <SDL2/SDL.h> 
+#include <SDL2/SDL_image.h> 
+#include <SDL2/SDL_timer.h> 
+
 using namespace std;
 
-int main() {
+int main() 
+{ 
     cout << "nakki" <<endl;
-    return 0;
-}
+ 
+    // retutns zero on success else non-zero 
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { 
+        printf("ERR: Failed to initialize SDL: %s\n", SDL_GetError()); 
+    } 
+    SDL_Window* win = SDL_CreateWindow("Title goes here", 
+                                       SDL_WINDOWPOS_CENTERED, 
+                                       SDL_WINDOWPOS_CENTERED, 
+                                       800, 600, 0); 
+
+    int running = 1;
+
+    // annimation loop 
+    while (running) { 
+        SDL_Event event; 
+  
+        // Events mangement 
+        while (SDL_PollEvent(&event)) { 
+            switch (event.type) { 
+  
+            case SDL_QUIT: 
+                // handling of close button 
+                running = 0; 
+                break;   
+            } 
+        } 
+    }
+
+    SDL_Quit();
+    return 0; 
+} 

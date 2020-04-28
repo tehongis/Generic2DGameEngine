2d0afaba922dec26d01ce88f9fda73592f6c37e1
Tero Hongisto <5723645+tehongis@users.noreply.github.com>
2020-04-28 21:34:53 +0300

    Sprites floating and rotating now.

--- SDLEngine.cpp
+++ SDLEngine.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
+#include <cmath>
 
-#include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
+#include <SDL2/SDL.h>
 #undef main
 
 #include <Box2D/Box2D.h>
@@ -12,34 +13,46 @@
 
 #define WIDTH 1280
 #define HEIGHT 1024
+#define CX WIDTH/2
+#define CY HEIGHT/2
 
 int main() {
+
+  int running = 1;      //Assuming everything will go fine, this well keep as 1.
 
   SDL_Window *window = NULL;
   SDL_Renderer *renderer = NULL;
   SDL_Texture *sprite = NULL;
   int w, h;
 
+  float countA = 0.0;
+  float countB = 0.4;
+  float countC = 1.1;
+
   b2Vec2 gravity(0.0f, -10.0f);
   b2World myWorld = b2World(gravity);
 
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     printf("ERR: Failed to initialize SDL: %s\n", SDL_GetError());
+    running = 0;
   }
   window = SDL_CreateWindow("Title goes here", SDL_WINDOWPOS_CENTERED,
                             SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
   renderer = SDL_CreateRenderer(window, -1, 0);
-
-  sprite = IMG_LoadTexture(renderer, "sprite.png");
+  if (renderer == NULL) {
+      printf("ERR: Renderer failed.\n", SDL_GetError());
+      running = 0;
+  }
+    
+  sprite = IMG_LoadTexture(renderer, "images/sprite.png");
+  if (sprite == NULL) {
+      printf("ERR: Imageload failed: %s\n", SDL_GetError());
+      running = 0;
+  }
+  
   SDL_QueryTexture(sprite, NULL, NULL, &w, &h);
   SDL_Rect texr;
-  texr.x = WIDTH / 2;
-  texr.y = HEIGHT / 2;
-  texr.w = w * 2;
-  texr.h = h * 2;
-
-  int running = 1;
 
   SDL_Event event;
 
@@ -63,10 +76,36 @@ int main() {
     SDL_SetRenderDrawColor(renderer, 30, 40, 50, 255);
     SDL_RenderClear(renderer);
 
-    SDL_RenderCopy(renderer, sprite, NULL, &texr);
+    countA = countA + 0.03;
+    if (countA > M_PI * 2) {
+        countA = countA - M_PI * 2;
+    }
+
+    countB = countB + 0.027;
+    if (countB > M_PI * 2) {
+        countB = countB - M_PI * 2;
+    }
+
+    countC = countC + 0.043;
+    if (countC > M_PI * 2) {
+        countC = countC - M_PI * 2;
+    }
+
+
+    for (int i = 0; i < 512; i++) {
+        float f = (i / 100.0);
+        texr.x = int( CX + 200 * ( sin( countA + f ) + sin( countB + f ) + sin(countC + f) ) );
+        texr.y = int( CY + 200 * ( cos( countA + f ) + sin( countB + f ) + sin(countC + f) ) );
+        texr.w = w * 2;
+        texr.h = h * 2;
+
+        // SDL_RenderCopy(renderer, sprite, NULL, &texr);
+        double angle = f + countA * 360.0;
+        SDL_RenderCopyEx(renderer, sprite, NULL, &texr, angle, NULL, SDL_FLIP_NONE);
+    }
 
     SDL_RenderPresent(renderer);
-    SDL_Delay(100);
+    SDL_Delay(25);
   }
 
   SDL_DestroyTexture(sprite);
